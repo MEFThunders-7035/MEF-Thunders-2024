9c8b8ef7a5fd4ca0b8f5d8a608754ff41a602d6b
AlganOngun <alganunlu1896@gmail.com>
2024-01-10 09:47:28 +0300

    Linux and neovim entegration

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,7 +34,7 @@ public final class Constants {
             public static final int kFrontLeftTurningCanID = 2;
 
             public static final int kFrontRightDrivingCanID = 3;
-            public static final int kFrontRightTurningCanID = 4;            
+            public static final int kFrontRightTurningCanID = 4;
 
             public static final int kRearLeftDrivingCanID = 5;
             public static final int kRearLeftTurningCanID = 6;
@@ -47,7 +47,7 @@ public final class Constants {
             public static final double kFreeSpeedRpm = 5676;
         }
 
-        
+
         public static final class SwerveModuleConstants {
             // Angular offsets of the modules relative to the chassis in radians
             public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
@@ -68,11 +68,11 @@ public final class Constants {
             // This changes the drive speed of the module (a pinion gear with more teeth will result in a
             // robot that drives faster).
             public static final int kDrivingMotorPinionTeeth = 14;
-        
+
             // Invert the turning encoder, since the output shaft rotates in the opposite direction of
             // the steering motor in the MAXSwerve Module.
             public static final boolean kTurningEncoderInverted = true;
-        
+
             // Calculations required for driving motor conversion factors and feed forward
             public static final double kDrivingMotorFreeSpeedRps = DriveConstants.NeoMotorConstants.kFreeSpeedRpm / 60;
             public static final double kWheelDiameterMeters = 0.0762;
@@ -81,35 +81,35 @@ public final class Constants {
             public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
             public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
                 / kDrivingMotorReduction;
-        
+
             public static final double kDrivingEncoderPositionFactor = (kWheelDiameterMeters * Math.PI)
                 / kDrivingMotorReduction; // meters
             public static final double kDrivingEncoderVelocityFactor = ((kWheelDiameterMeters * Math.PI)
                 / kDrivingMotorReduction) / 60.0; // meters per second
-        
+
             public static final double kTurningEncoderPositionFactor = (2 * Math.PI); // radians
             public static final double kTurningEncoderVelocityFactor = (2 * Math.PI) / 60.0; // radians per second
-        
+
             public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
             public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
-        
+
             public static final double kDrivingP = 0.04;
             public static final double kDrivingI = 0;
             public static final double kDrivingD = 0;
             public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
             public static final double kDrivingMinOutput = -1;
             public static final double kDrivingMaxOutput = 1;
-        
+
             public static final double kTurningP = 1;
             public static final double kTurningI = 0;
             public static final double kTurningD = 0;
             public static final double kTurningFF = 0;
             public static final double kTurningMinOutput = -1;
             public static final double kTurningMaxOutput = 1;
-        
+
             public static final IdleMode kDrivingMotorIdleMode = IdleMode.kBrake;
             public static final IdleMode kTurningMotorIdleMode = IdleMode.kBrake;
-        
+
             public static final int kDrivingMotorCurrentLimit = 50; // amps
             public static final int kTurningMotorCurrentLimit = 20; // amps
         }

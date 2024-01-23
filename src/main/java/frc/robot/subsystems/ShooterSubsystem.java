048fb2ca117ac11cb7ea533ea06b4a1f4708cecc
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-01-23 16:07:54 +0300

    Change back to Spark for now
    REVERT THIS COMMIT BEFORE MERGE!

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,23 +1,17 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ShooterConstants;
 
 // ! WARNING: This code will be changed to use 2 sparkMAXes with encoders and 1 being a follower.
 public class ShooterSubsystem extends SubsystemBase {
-  private final CANSparkMax shooterMotor1;
-  private final CANSparkMax shooterMotor2;
+  private final Spark shooterMotor;
 
   public ShooterSubsystem() {
-    shooterMotor1 = new CANSparkMax(ShooterConstants.kShooter1CanID, MotorType.kBrushed);
-    shooterMotor2 = new CANSparkMax(ShooterConstants.kShooter2CanID, MotorType.kBrushed);
-
-    shooterMotor2.follow(shooterMotor1);
+    shooterMotor = new Spark(0);
   }
 
   public void setShooterSpeed(double speed) {
-    // USE PID CONTROLLER
+    shooterMotor.set(speed);
   }
 }

94fcbe9d4892e90d765d943499d862d15decc8f2
MEFThunders7035 <157488669+MEFThunders7035@users.noreply.github.com>
2024-02-26 09:40:50 +0300

    add Photon Sim (!WARN: DO NOT USE, IN PROGRESS)
    Co-Authored-By: Kaya <95276965+kytpbs@users.noreply.github.com>

---
+++ src/main/java/frc/robot/simulationSystems/PhotonSim.java
@@ -0,0 +1,31 @@
+package frc.robot.simulationSystems;
+
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import frc.robot.subsystems.PhotonCameraSystem;
+import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.simulation.VisionSystemSim;
+
+public class PhotonSim {
+  // TODO: Implement PhotonSim
+  private static PhotonCameraSim cameraSim;
+  private static VisionSystemSim visionSim;
+
+  public static void setupSim() {
+    cameraSim = new PhotonCameraSim(PhotonCameraSystem.getCamera());
+    visionSim = new VisionSystemSim(cameraSim.getCamera().getName());
+    visionSim.addCamera(cameraSim, new Transform3d());
+    visionSim.addAprilTags(AprilTagFields.k2024Crescendo.loadAprilTagLayoutField());
+  }
+
+  public static void update(Pose2d robotPose) {
+    visionSim.update(robotPose);
+  }
+
+  public static PhotonCameraSim getCameraSim() {
+    return cameraSim;
+  }
+
+  public static void addAprilTagWithID(int id) {}
+}

78326c8eac48f6195390b04d60f28bdb3162406e
MEFThunders7035 <95276965+kytpbs@users.noreply.github.com>
2024-03-28 02:20:17 +0300

    change wait time to a seperate variable
    TODO: move it to Constants.java

--- src/main/java/frc/robot/commands/SmartShootCommand.java
+++ src/main/java/frc/robot/commands/SmartShootCommand.java
@@ -8,9 +8,11 @@ import frc.robot.subsystems.ShooterSubsystem;
 import java.util.function.DoubleSupplier;
 
 public class SmartShootCommand extends ParallelRaceGroup {
+  private static final double waitTime = 2.5;
+
   public SmartShootCommand(ShooterSubsystem shooterSubsystem, IntakeSubsystem intakeSubsystem) {
     super(
-        new BasicRunShooterCommand(shooterSubsystem, 1)
+        new BasicRunShooterCommand(shooterSubsystem, waitTime)
             .andThen(
                 // This command is required, so the default command doesn't override it to 0.
                 new BasicRunShooterCommand(shooterSubsystem)
@@ -26,7 +28,7 @@ public class SmartShootCommand extends ParallelRaceGroup {
         // Constantly move the arm until all the other commands finish.
         new MoveArmToShooterCommand(armSubsystem, driveSubsystem)
             .raceWith(
-                new BasicRunShooterCommand(shooterSubsystem, 2.5)
+                new BasicRunShooterCommand(shooterSubsystem, waitTime)
                     .andThen(
                         new BasicRunShooterCommand(shooterSubsystem)
                             .raceWith(intakeSubsystem.loadToShooterCommand()))));
@@ -44,7 +46,7 @@ public class SmartShootCommand extends ParallelRaceGroup {
         new MoveArmToShooterCommand(armSubsystem, driveSubsystem),
         new DriveFacingShooter(driveSubsystem, xSpeed, ySpeed),
         // The finishing command will be this one:
-        new BasicRunShooterCommand(shooterSubsystem, 2.5)
+        new BasicRunShooterCommand(shooterSubsystem, waitTime)
             .andThen(
                 new BasicRunShooterCommand(shooterSubsystem)
                     .raceWith(intakeSubsystem.loadToShooterCommand())));

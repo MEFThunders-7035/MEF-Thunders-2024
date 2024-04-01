08af750be64dbbe707957a7f3388d2002908f73a
MEFThunders7035 <95276965+kytpbs@users.noreply.github.com>
2024-04-01 21:05:55 +0300

    change LEDSystem to hold a singleton of led subsystem
    This will offload most of my work to at least the main command scheduler, now; all I need to do is handle the led scheduler only. and that implementation is basic anyways

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,14 +1,91 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDConstants;
 import frc.utils.BetterLED;
 
 public class LEDSubsystem extends SubsystemBase {
   BetterLED strip;
+  Color lastSetColor = new Color();
+  Color lastSetBlinkingColor = new Color();
+  boolean isBlinkingRed = false;
 
   public LEDSubsystem() {
     strip = new BetterLED(LEDConstants.kLedPin, LEDConstants.kLedCount);
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putString("LED Currently Running", strip.getCurrentCommandName());
+  }
+
+  public void fill(Color color) {
+    if (lastSetColor.equals(color)) {
+      return;
+    }
+    strip.fillColor(color);
+    lastSetColor = color;
+    isBlinkingRed = false;
+  }
+
+  public void fill(Color color, int count) {
+    fill(color, count, false);
+  }
+
+  public void fill(Color color, int count, boolean force) {
+    if (lastSetColor.equals(color) && !force) {
+      return;
+    }
+    strip.fillColor(color, count);
+    lastSetColor = color;
+  }
+
+  public Command getRedBlinkCommand() {
+    return this.run(this::blinkRed)
+        .finallyDo(
+            () -> {
+              strip.removeFromLoop();
+              lastSetBlinkingColor = new Color();
+            });
+  }
+
+  public void setRed() {
+    fill(Color.kRed);
+  }
+
+  public void setGreen() {
+    fill(Color.kGreen);
+  }
+
+  public void setStatusColor(boolean status) {
+    if (status) {
+      setGreen();
+    } else {
+      setRed();
+    }
+  }
+
+  public void blink(Color color) {
+    if (lastSetBlinkingColor.equals(color)) {
+      return;
+    }
+    lastSetColor = new Color(); // Reset back to 0
+    lastSetBlinkingColor = color;
+    strip.blink(color);
+  }
+
+  public void blinkRed() {
+    if (isBlinkingRed) {
+      return;
+    }
+    blink(new Color(255, 0, 0));
+  }
+
+  public void fillPercentageWithColor(double percentage, Color color) {
+    fill(color, (int) (strip.getLedCount() * percentage), true);
   }
 
   public BetterLED getStrip() {
--- src/main/java/frc/robot/subsystems/LEDSystem.java
+++ src/main/java/frc/robot/subsystems/LEDSystem.java
@@ -1,51 +1,21 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.util.Color;
-import frc.robot.Constants.LEDConstants;
-import frc.utils.BetterLED;
-import java.util.function.BooleanSupplier;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public final class LEDSystem {
-  private static final BetterLED strip =
-      new BetterLED(LEDConstants.kLedPin, LEDConstants.kLedCount);
-  private static boolean isRunningACommand = false;
+  private static final LEDSubsystem ledSubsystemInstance = new LEDSubsystem();
 
-  public static void init() {
-    System.out.println("Startup LED System!");
+  private LEDSystem() {}
+
+  public static LEDSubsystem getInstance() {
+    return ledSubsystemInstance;
   }
 
-  public static void setLEDColorRGB(int r, int g, int b) {
-    strip.fill(new Color(r, g, b));
+  public static Command run(Runnable toRun) {
+    return ledSubsystemInstance.run(toRun);
   }
 
-  public static boolean isRunningACommand() {
-    return isRunningACommand;
-  }
-
-  public static void setRunningACommand(boolean isRunningACommand) {
-    LEDSystem.isRunningACommand = isRunningACommand;
-  }
-
-  public static void addTimedCommand(double timeSeconds) {
-    isRunningACommand = true;
-    new Thread(
-            () -> {
-              Timer.delay(timeSeconds);
-              isRunningACommand = false;
-            })
-        .start();
-  }
-
-  public static void addBooleanSupplierCommand(BooleanSupplier hasEnded) {
-    isRunningACommand = true;
-    new Thread(
-            () -> {
-              while (!hasEnded.getAsBoolean()) {
-                Timer.delay(0.1);
-              }
-              isRunningACommand = false;
-            })
-        .start();
+  public static Command getBlinkRedCommand() {
+    return ledSubsystemInstance.getRedBlinkCommand();
   }
 }

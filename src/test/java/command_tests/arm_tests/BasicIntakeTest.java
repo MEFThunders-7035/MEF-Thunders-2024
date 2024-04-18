58c65f9396946c1db7a49b0430dedde935ae0e22
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-04-19 01:08:53 +0300

    move basic intake test to arm_tests folder

--- src/test/java/command_tests/arm_tests/BasicIntakeTest.java
+++ src/test/java/command_tests/arm_tests/BasicIntakeTest.java
@@ -1,51 +1,40 @@
-package command_tests;
+package command_tests.arm_tests;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static utils.LEDTestUtils.checkForColorInAll;
 
 import com.revrobotics.CANSparkMax;
+import command_tests.utils.CommandTestBase;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.commands.BasicIntakeCommand;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.LEDSubsystem;
-import frc.robot.subsystems.LEDSystem;
 import frc.utils.sim_utils.SparkMAXSimAddon;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BasicIntakeTest {
+class BasicIntakeTest extends CommandTestBase {
   private IntakeSubsystem intakeSubsystem;
+
   private CANSparkMax intakeMotor;
-  private LEDSubsystem ledSubsystem;
   private BasicIntakeCommand intakeCommand;
-  private CommandScheduler commandScheduler;
 
   @BeforeEach
   public void setUp() {
+    super.setUp();
     intakeSubsystem = new IntakeSubsystem();
     intakeMotor = SparkMAXSimAddon.getSparkMAX(IntakeConstants.kArmIntakeMotorCanID);
-    ledSubsystem = LEDSystem.getInstance();
     intakeCommand = new BasicIntakeCommand(intakeSubsystem);
 
-    // Enable robot for commands to run
-    DriverStationSim.setEnabled(true);
-    DriverStationSim.setDsAttached(true);
-    DriverStationSim.notifyNewData(); // ! Breaks without this
-    commandScheduler = CommandScheduler.getInstance();
     commandScheduler.schedule(intakeCommand);
   }
 
   @AfterEach
   public void tearDown() {
-    LEDSystem.resetLEDSubsystem();
     intakeSubsystem.close();
-    intakeCommand.cancel();
-    commandScheduler.close();
+    super.tearDown();
   }
 
   @Test

a825852806b96f8c752e016f9e91842a59de6ace
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-04-18 18:54:06 +0300

    add led tests

---
+++ src/test/java/utils/LEDTestUtils.java
@@ -0,0 +1,19 @@
+package utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.subsystems.LEDSubsystem;
+
+public class LEDTestUtils {
+  public static void checkForColorInAll(
+      LEDSubsystem ledSubsystem, Color colorItShouldBe, String message) {
+    for (int i = 0; i < ledSubsystem.getLedCount(); i++) {
+      assertEquals(colorItShouldBe, getColorAtIndex(ledSubsystem, i), message);
+    }
+  }
+
+  public static Color getColorAtIndex(LEDSubsystem ledSubsystem, int index) {
+    return ledSubsystem.getStrip().getBuffer().getLED(index);
+  }
+}

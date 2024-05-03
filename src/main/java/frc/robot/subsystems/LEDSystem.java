0eb2120b38ff0d195389845e23fe7221ff0deb6c
MEFThunders7035 <95276965+kytpbs@users.noreply.github.com>
2024-05-03 23:05:02 +0300

    add syntatic sugar to LEDSystem
    LEDSystem will be repaced by making LEDSubsystem a singleton instead... but I wonder when

--- src/main/java/frc/robot/subsystems/LEDSystem.java
+++ src/main/java/frc/robot/subsystems/LEDSystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public final class LEDSystem {
@@ -17,6 +18,10 @@ public final class LEDSystem {
 
   public static Command getBlinkRedCommand() {
     return ledSubsystemInstance.getRedBlinkCommand();
+  }
+
+  public static Command getBlinkColorCommand(Color color) {
+    return ledSubsystemInstance.getBlinkColorCommand(color);
   }
 
   public static void resetLEDSubsystem() {

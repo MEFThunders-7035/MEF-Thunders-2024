95513b03838f56b062d25c89e2f651baf4deb2cd
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-05-16 06:42:55 +0300

    Create a test base for DriveSubsystem
    this should make things a lot easier ngl

---
+++ src/test/java/subsystem_tests/drive_subsystem_tests/DriveSubsystemTestBase.java
@@ -0,0 +1,30 @@
+package subsystem_tests.drive_subsystem_tests;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.DriveSubsystem;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class DriveSubsystemTestBase {
+  protected DriveSubsystem driveSubsystem;
+  protected CommandScheduler commandScheduler;
+
+  @BeforeEach
+  public void setUp() {
+    assert HAL.initialize(500, 0);
+    commandScheduler = CommandScheduler.getInstance();
+    driveSubsystem = new DriveSubsystem();
+
+    // Enable robot for commands to run
+    DriverStationSim.setEnabled(true);
+    DriverStationSim.setDsAttached(true);
+    DriverStationSim.notifyNewData(); // ! Breaks without this
+  }
+
+  @AfterEach
+  public void tearDown() {
+    driveSubsystem.close();
+  }
+}

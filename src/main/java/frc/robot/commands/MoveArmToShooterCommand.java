9a93e1b1cd2292ff5a3dcd1737f694ad30f14462
MEFThunders7035 <95276965+kytpbs@users.noreply.github.com>
2024-05-20 23:43:26 +0300

    Make Move Arm To Shooter Command a never ending command instead

--- src/main/java/frc/robot/commands/MoveArmToShooterCommand.java
+++ src/main/java/frc/robot/commands/MoveArmToShooterCommand.java
@@ -1,13 +1,17 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.utils.ExtraFunctions;
 
-public class MoveArmToShooterCommand extends MoveArmToPositionCommand {
+public class MoveArmToShooterCommand extends RunCommand {
+  /** This Command Never Ends! */
   public MoveArmToShooterCommand(ArmSubsystem armSubsystem, DriveSubsystem driveSubsystem) {
     super(
-        armSubsystem,
-        () -> ExtraFunctions.getAngleFromDistance(driveSubsystem.getDistanceToShooter()));
+        () ->
+            armSubsystem.setArmToPosition(
+                ExtraFunctions.getAngleFromDistance(driveSubsystem.getDistanceToShooter())),
+        armSubsystem);
   }
 }
